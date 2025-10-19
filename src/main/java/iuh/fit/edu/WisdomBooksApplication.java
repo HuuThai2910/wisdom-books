7e0c62052b2a29409d578bfce225913e6b730974
HuuThai <levanhuuthai2004@gmail.com>
2025-10-19 12:18:56 +0700

    first commit

---
+++ src/main/java/iuh/fit/edu/WisdomBooksApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisdomBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/edu/WisdomBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
